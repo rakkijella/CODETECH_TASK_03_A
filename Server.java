bdf46e1ece595947327c2b5cc94e7cab10dd9d62
Rakesh Jella <rbgaming2024@gmail.com>
2025-04-10 14:45:33 +0530

    Server.java

---
+++ Server.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Server{
+    private static final int PORT = 1234;
+    private static Set<ClientHandler> clientHandlers = new HashSet<>();
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        System.out.println("Server started on port " + PORT);
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("New client connected: " + clientSocket);
+            ClientHandler handler = new ClientHandler(clientSocket);
+            clientHandlers.add(handler);
+            new Thread(handler).start();
+        }
+    }
+
+    public static void broadcast(String message, ClientHandler sender) {
+        for (ClientHandler client : clientHandlers) {
+            if (client != sender) {
+                client.sendMessage(message);
+            }
+        }
+    }
+
+    public static void removeClient(ClientHandler handler) {
+        clientHandlers.remove(handler);
+    }
+
+    static class ClientHandler implements Runnable {
+        private Socket socket;
+        private PrintWriter out;
+        private BufferedReader in;
+
+        public ClientHandler(Socket socket) throws IOException {
+            this.socket = socket;
+            this.out = new PrintWriter(socket.getOutputStream(), true);
+            this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        }
+
+        public void sendMessage(String message) {
+            out.println(message);
+        }
+
+        public void run() {
+            try {
+                String message;
+                while ((message = in.readLine()) != null) {
+                    System.out.println("Received: " + message);
+                    Server.broadcast(message, this);
+                }
+            } catch (IOException e) {
+                System.out.println("Client disconnected.");
+            } finally {
+                try {
+                    socket.close();
+                    Server.removeClient(this);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
